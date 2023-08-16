28b7125ec8f4b14ce93f9229f91cec1fb26bf6eb
awais <awais.mansoor@gosaas.io>
2023-08-16 13:19:52 +0500

    end of section 19

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -31,7 +31,11 @@ namespace API.Extensions
             });
             services.AddCors(opt => {
                 opt.AddPolicy("CorsPolicy", policy => {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:5173");
+                    policy
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials()
+                        .WithOrigins("http://localhost:5173");
                 });
             });
             services.AddMediatR(typeof(List.Handler));
@@ -42,6 +46,7 @@ namespace API.Extensions
             services.AddScoped<IUserAccessor, UserAccessor>();
             services.AddScoped<IPhotoAccessor, PhotoAccessor>();
             services.Configure<CloudinarySettings>(config.GetSection("Cloudinary"));
+            services.AddSignalR();
 
             return services;
         }
---
+++ Application/Comments/Create.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Core;
+using Application.Interfaces;
+using AutoMapper;
+using Domain;
+using FluentValidation;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Comments
+{
+    public class Create
+    {
+        public class Command : IRequest<Result<CommentDto>>
+        {
+            public string Body { get; set; }
+            public Guid ActivityId { get; set; }
+        }
+
+        public class CommentValidator : AbstractValidator<Comment>
+        {
+            public CommentValidator()
+            {
+                RuleFor(x => x.Body).NotEmpty();
+            }
+        }
+
+        public class Handler : IRequestHandler<Command, Result<CommentDto>>
+        {
+            private readonly IUserAccessor _userAccessor;
+            private readonly IMapper _mapper;
+            private readonly DataContext _context;
+            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
+            {
+                _context = context;
+                _mapper = mapper;
+                _userAccessor = userAccessor;
+            }
+
+            public async Task<Result<CommentDto>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.ActivityId);
+
+                if(activity == null) return null;
+
+                var user = await _context.Users
+                    .Include(p => p.Photos)
+                    .SingleOrDefaultAsync(p => p.UserName == _userAccessor.GetUsername());
+
+                var comment = new Comment
+                {
+                    Author = user,
+                    Activity = activity,
+                    Body = request.Body
+                };
+
+                activity.Comments.Add(comment);
+
+                var success = await _context.SaveChangesAsync() > 0;
+
+                return success
+                    ? Result<CommentDto>.Success(_mapper.Map<CommentDto>(comment))
+                    : Result<CommentDto>.Failure("Problem adding comment to activity");
+            }
+        }
+    }
+}
